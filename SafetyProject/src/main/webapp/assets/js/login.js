5debfd649cb18765dbaeac91b737c62d03311ece
DebajyotiGoswami <goswamidiary@gmail.com>
2024-09-02 15:36:14 +0530

    new project folder created. huge validation in userid and password has been made. code is running successfully.

---
+++ SafetyProject/src/main/webapp/assets/js/login.js
@@ -0,0 +1,183 @@
+var url = "http://10.251.37.170:8080/testSafety/testSafety";
+
+function isValidNumber(input) {
+	const numericRegex = /^[0-9]+$/;
+	return numericRegex.test(input) && input.length === 8 && input.startsWith('9');
+}
+function isValidOtp(input) {
+	const numericRegex = /^[0-9]+$/;
+	return numericRegex.test(input) && input.length == 5
+}
+function validateOtpAndSubmit(otp) {
+	const errorDisplay = document.getElementById('otpError');
+	if (!isValidOtp(otp)) {
+		errorDisplay.textContent = 'OTP must be numeric and should be 5 digits long.';
+		return false;
+	}
+	errorDisplay.textContent = "";
+	return true;
+}
+function validateLoginAndSubmit(User, Pwd) {
+	const userId = User;
+	const password = Pwd;
+	const errorDisplay = document.getElementById('loginError');
+	if (!isValidNumber(userId)) {
+		errorDisplay.textContent = 'User ID must be numeric, start with 9, and be 8 digits long.';
+		return;
+	}
+	if (userId === password) {
+		errorDisplay.textContent = 'User ID and Password must not match.';
+		return;
+	}
+	errorDisplay.textContent = '';
+	return true;
+}
+function enableResendButton() {
+	const resendButton = document.getElementById('resendOtpBtn');
+	resendButton.disabled = false;
+	resendButton.textContent = 'Resend OTP';
+}
+function startResendOtpTimer() {
+	const resendButton = document.getElementById('resendOtpBtn');
+	resendButton.disabled = true;
+	let countdown = 5;
+	resendButton.textContent = "Resend OTP (" + String(countdown) + "s)";
+
+	const interval = setInterval(() => {
+		countdown--;
+		resendButton.textContent = `Resend OTP (${countdown}s)`;
+
+		if (countdown === 0) {
+			clearInterval(interval);
+			enableResendButton();
+		}
+	}, 1000);
+}
+
+function setCookie(name, value, minutes) {
+	let expires = "";
+	if (minutes) {
+		const date = new Date();
+		date.setTime(date.getTime() + (minutes * 60 * 1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+	const nameEQ = name + "=";
+	const ca = document.cookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+$(document).ready(function() {
+	$('#loginbttn, #submitOtpBtn, #resendOtpBtn').on('click', handleButtonClick);
+});
+
+function handleButtonClick(event) {
+	const buttonId = event.target.id;
+	var loginflg = false;
+	var submitotpflg = false;
+	var resendotpflg = false;
+	var jsonObj = {};
+	var User = $('#userId').val();
+
+	if (buttonId === 'loginbttn') {
+		var Pwd = $('#password').val();
+		if (validateLoginAndSubmit(User, Pwd)) {
+			loginflg = true;
+			var userAgent = navigator.userAgent;
+			var userAgent = userAgent.replace(/[^\w\s.]/g, " "); //replace all symbols with space
+			jsonObj = {
+				"User": User,
+				"Pwd": Pwd,
+				"userAgent": userAgent,
+				"pageNm": "LOGIN"
+			};
+		} else {
+			return;
+		}
+	}
+	else {
+		var otp = $('#otp').val();
+		jsonObj = {
+			"User": User,
+			"otp": otp,
+			"pageNm": "OTP",
+			"xUid": xUid,
+			"empDtls": JSON.parse(getCookie("empDtls"))
+		};
+		if (buttonId === 'submitOtpBtn') {
+			submitotpflg = true;
+			if (validateOtpAndSubmit(otp)) {
+				startResendOtpTimer();
+			} else {
+				return;
+			}
+		}
+		else if (buttonId === 'resendOtpBtn') {
+			jsonObj["pageNm"]= "RESOTP";
+			resendotpflg = true;
+		}
+	}
+
+	$.ajax({
+		url: url, // replace with above Servlet URL
+		type: 'POST',
+		data: JSON.stringify(jsonObj),
+		success: function(response) {
+			if (loginflg) {
+				if (response.ackMsgCode == '105') {
+					xUid = response.xUid;
+					setCookie("empDtls", JSON.stringify(response.empDtls), 30);
+					// Show OTP section
+					$('#otpMessage').show();
+					$('#otpForm').show();
+					$('#otp').prop('disabled', false);
+					$('#submitOtpBtn').prop('disabled', false);
+					$('#loginbttn').prop('disabled', true);
+					startResendOtpTimer();
+				} else {
+					alert("Incorrect credentials. Please try again.");
+				}
+			} else {
+				var empDtls = {
+					"erpId": User,
+					"name": response.empDtls.EMNAMCL,
+					"office": response.empDtls.LTEXTCL,
+					"designation": response.empDtls.STEXTCL,
+					"role": response.role,
+					"xUid": response.xUid,
+					"tkn": response.tkn
+				};
+				if (submitotpflg) {
+					if (response.ackMsgCode == '100') {
+						window.location.href = 'dashboard.jsp';
+						setCookie("empDtls", empDtls, 30);
+					} else {
+						alert("Incorrect OTP. Please check the OTP and try again.");
+					}
+				} else if (resendotpflg) {
+					if (response.ackMsgCode == '100') {
+						document.getElementById('resendOtp').disabled = true;
+						window.location.href = 'dashboard.jsp';
+					} else {
+						alert("OTP resent. Please check your registered mobile number.");
+					}
+				}
+			}
+		}, error: function(xhr, status, error) {
+			//if server not get connected 
+			console.error("xhr: " + JSON.stringify(xhr) + "\nstatus: " + status + "\nerror: " + error);
+		}
+	});
+
+
+}
+
+
---
+++ SafetyProject/src/main/webapp/login.jsp
@@ -0,0 +1,100 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Safety Application</title>
+
+<!-- Link to Bootstrap CSS -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+<!-- Link to custom CSS -->
+<link rel="stylesheet" href="assets/css/before_login.css">
+
+<!-- jQuery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- JavaScript to handle LDAP authentication and OTP submission -->
+<script src="assets/js/login.js"></script>
+</head>
+
+<body>
+	<!-- Header -->
+	<nav class="navbar navbar-expand-lg navbar-dark">
+		<a class="navbar-brand" href="#"> <img
+			src="assets/images/logo.png" alt="Logo">
+		</a> <span class="navbar-brand">West Bengal State Electricity
+			Distribution Company Limited</span>
+	</nav>
+
+	<!-- Welcome Content -->
+	<div class="container login-container mt-5">
+		<div class="text-center header-title mb-4">
+			<h2>Safety Inspection Portal</h2>
+		</div>
+
+		<!-- Login form -->
+		<div class="form-row">
+			<div class="form-group col-md-12">
+				<label for="userId" class="mr-2">User ID</label> <input type="text"
+					class="form-control" id="userId" name="userId"
+					placeholder="Enter your ERP ID" required>
+					
+			</div>
+		</div>
+
+		<!-- Password section -->
+		<div class="form-row">
+			<div class="form-group col-md-12">
+				<label for="password" class="mr-2">Password</label> <input
+					type="password" class="form-control" id="password" name="password"
+					placeholder="Enter your password" required>
+					 
+			</div>
+		</div>
+		<p id="loginError" class="error"></p>
+		<!-- Login Button -->
+		<div class="form-row">
+			<div class="form-group col-md-12 d-flex justify-content-center">
+				<button type="button" class="btn btn-primary" id="loginbttn"
+					style="width: 50%;">Login</button>
+			</div>
+		</div>
+
+		<!-- OTP Sent Message -->
+		<div class="form-row" id="otpMessage" style="display: none;">
+			<div class="form-group col-md-12 text-center">
+				<p class="text-info">A one-time password has been sent to your
+					registered mobile number.</p>
+			</div>
+		</div>
+
+		<!-- OTP Section -->
+		<form id="otpForm" style="display: none;">
+			<div class="form-row">
+				<div class="form-group col-md-12">
+					<label for="otp" class="mr-2">OTP</label> <input type="text"
+						class="form-control" id="otp" name="otp" placeholder="Enter OTP"
+						required disabled>
+				</div>
+				<div class="form-group col-md-12 d-flex justify-content-center">
+					<button type="button" class="btn btn-primary" id="submitOtpBtn"
+						style="width: 50%;" disabled>Submit
+						OTP</button>&nbsp;
+						<button type="button" class="btn btn-primary" id="resendOtpBtn"
+						style="width: 50%;" disabled>
+						</button>
+				</div>
+				<p id="otpError" class="error"></p>
+<!-- 				<div id="otpError" class="alert alert-danger" role="alert"
+					style="display: none;"></div> -->
+			</div>
+		</form>
+	</div>
+
+	<!-- Footer -->
+	<div class="footer">© 2024 WBSEDCL</div>
+
+
+</body>
+</html>
